eca59d8be02ab555658a73301ad56eb5c45818b7
velazquezfacu <facujunior1@gmail.com>
2025-10-14 12:10:02 -0300

    imagen ico
    agregue la imagen .ico

--- sesion.php
+++ sesion.php
@@ -8,9 +8,9 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lic. Carla Cuenca - Iniciar sesion</title>
+    <title>Club de Socios C.A.I - Iniciar Sesión</title>
     <!-- Favicon -->
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="images/escudocai.ico" type="image/x-icon">
     <!-- Custom CSS Link -->
     <link rel="stylesheet" href="styles/styles.css">
     <!-- Google Font Link -->
