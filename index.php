bbf7d6bfab32731e224d6ecda7c2e9c69f484d26
velazquezfacu <facujunior1@gmail.com>
2025-11-04 19:46:59 -0300

    prueba

--- index.php
+++ index.php
@@ -41,10 +41,7 @@ $userName = $isLoggedIn ? $_SESSION['nombre'] : '';
                 <li><a href="#home">Inicio</a></li>
                 <li><a href="#servicios">Servicios</a></li>
                 <li><a href="#recetas">Ustedes</a></li>
-<<<<<<< HEAD
                 <li><a href="experiencias.php">Experiencias</a></li>
-=======
->>>>>>> 30a99fade09102c1bbf1a6819411d3849d0b5153
                 <li><a href="#contacto">Contacto</a></li>
                 <?php if ($isLoggedIn): ?>
                     <li class="user-menu">
@@ -251,11 +248,7 @@ $userName = $isLoggedIn ? $_SESSION['nombre'] : '';
                         <b>Costo:</b> $50.000 ARS <br>
                         <b>Renovación:</b> Mensual <br>                    
                     </p>
-<<<<<<< HEAD
                     <h3><b>Beneficios Adicionaless: (incluye todos los anteriores)</b></h3>
-=======
-                    <h3><b>Beneficios Adicionales: (incluye todos los anteriores)</b></h3>
->>>>>>> 30a99fade09102c1bbf1a6819411d3849d0b5153
                     <ul>
                         <li><b>-Descuento del 20%</b> en la cuota de la Sede Social y actividades.</li>
                         <li><b>-Descuento fijo del 15%</b> en la tienda oficial del club.</li>
